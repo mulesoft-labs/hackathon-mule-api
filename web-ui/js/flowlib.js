0512f701d9b28567963ff0c23bccf01673cf21f9
farolfo <farolfo@mulesoft.com>
2013-04-18 20:10:23 -0300

    adding a web ui

---
+++ web-ui/js/flowlib.js
@@ -0,0 +1,39 @@
+$(function () {
+    var source   = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var $results = $('.results'),
+        $searchButton = $('#search button'),
+        $searchQuery = $('#search input');
+
+
+    var packagesResourceUrl = 'http://hackathon-mule-repository.herokuapp.com/api/packages';
+
+    function displayPackages(data) {
+        $results.html(template({items: data}));
+//        $resutlsTitle.
+    }
+
+    function findPackages(query){
+        var url = packagesResourceUrl;
+
+        if (query) {
+            url += '?q=' + encodeURIComponent(query);
+        }
+
+        return $.getJSON(url)
+    }
+
+
+    findPackages().then(displayPackages);
+    $searchQuery.keydown(function (evt) {
+        if (evt.keyCode === 13) {
+            $searchButton.click();
+        }
+    });
+    $searchButton.click(function () {
+        findPackages($searchQuery.val()).then(displayPackages);
+    });
+});
+
+
+
